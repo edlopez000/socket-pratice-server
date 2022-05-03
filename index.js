9892c04022bca56a92d0de2449f25cbc70e5b1ca
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-05-03 00:34:27 -0400

    Initial commti

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('<p>Hello world</p>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
+});
+
+server.listen(4001, () => {
+  console.log('Listening on port 4001');
+});
